c7a3c0a7a2cae26f7e29539d6f4fc65a1cbcd748
Minny27 <dltmdals0608@naver.com>
2021-09-17 13:51:03 +0900

    [#3]Docs: StudentViewModel 생성

--- ViewMdoel/StudentTableViewModel.swift
+++ ViewMdoel/StudentTableViewModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+final class StudentViewModel {
+    let studentList: [Student] = [
+        Student(name: "김승민", age: "15"),
+        Student(name: "이승민", age: "17")
+    ]
+    
+    var countStudentList: Int {
+        return studentList.count
+    }
+    
+    func studentInfo(at index: Int) -> Student {
+        return studentList[index]
+    }
+}
